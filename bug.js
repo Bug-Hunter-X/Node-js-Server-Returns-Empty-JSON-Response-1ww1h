b94d7292d02dcd2ea630a79f2b9a8e86e85ea27b
BugHunter-X <askzenai@gmail.com>
2025-01-24 12:30:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.writeHead(200);
+  response.end(JSON.stringify({})); //missing data
+};
+
+const server = http.createServer(requestListener);
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
